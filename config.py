ec209bce895314b98729a17fa1c6dda16b8f4746
Monu123a <monuahlawatxyx@gmail.com>
2026-01-18 16:42:18 +0530

    feat: add configuration management

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
 import os
 
-class Config:
-    DB_URL = os.getenv('DATABASE_URL', 'sqlite:///./data.db')
+class Settings:
+    PROJECT_NAME: str = 'Attendance Tracker'
+    DATABASE_URL: str = os.getenv('DATABASE_URL', 'sqlite:///./data.db')
+
+settings = Settings()
