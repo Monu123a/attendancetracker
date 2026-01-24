b36a53d755b8de963e179f5c80f3fcf2b03226a8
Monu123a <monuahlawatxyx@gmail.com>
2026-01-24 18:07:29 +0530

    feat: add AttendanceLog model

--- models.py
+++ models.py
@@ -1,4 +1,6 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+from datetime import datetime
 from database import Base
 
 class Student(Base):
@@ -6,3 +8,11 @@ class Student(Base):
     id = Column(Integer, primary_key=True, index=True)
     student_id = Column(String, unique=True, index=True)
     name = Column(String)
+    logs = relationship('AttendanceLog', back_populates='student')
+
+class AttendanceLog(Base):
+    __tablename__ = 'attendance_logs'
+    id = Column(Integer, primary_key=True, index=True)
+    student_id = Column(Integer, ForeignKey('students.id'))
+    timestamp = Column(DateTime, default=datetime.utcnow)
+    student = relationship('Student', back_populates='logs')
