b87f5f562d35693216ed41bb47201172f5b8be21
Monu123a <monuahlawatxyx@gmail.com>
2026-01-26 15:04:14 +0530

    feat: implement basic CRUD operations

--- crud.py
+++ crud.py
@@ -4,19 +4,9 @@ import models, schemas
 def get_student_by_id(db: Session, student_id: str):
     return db.query(models.Student).filter(models.Student.student_id == student_id).first()
 
-def get_all_students(db: Session):
-    return db.query(models.Student).all()
-
-def create_student(db: Session, payload: schemas.StudentCreate, enc: bytes = None):
-    st = models.Student(name=payload.name, student_id=payload.student_id, face_data=enc)
-    db.add(st)
+def create_student(db: Session, student: schemas.StudentCreate):
+    db_student = models.Student(name=student.name, student_id=student.student_id)
+    db.add(db_student)
     db.commit()
-    db.refresh(st)
-    return st
-
-def log_presence(db: Session, student_id: int):
-    log = models.AttendanceLog(student_id=student_id)
-    db.add(log)
-    db.commit()
-    db.refresh(log)
-    return log
+    db.refresh(db_student)
+    return db_student
