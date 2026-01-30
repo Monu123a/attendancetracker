2ba165f1578521b1f4e90de0b2ddcb4efc45598e
Monu123a <monuahlawatxyx@gmail.com>
2026-01-30 17:21:22 +0530

    fix: handle multi-face scenarios in frame

--- ai_service.py
+++ ai_service.py
@@ -6,5 +6,6 @@ def get_encodings(image_bytes):
     nparr = np.frombuffer(image_bytes, np.uint8)
     img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
     rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    encodings = face_recognition.face_encodings(rgb_img)
-    return encodings[0] if encodings else None
+    boxes = face_recognition.face_locations(rgb_img)
+    encodings = face_recognition.face_encodings(rgb_img, boxes)
+    return encodings
